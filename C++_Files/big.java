8cd73b4fb210d77bf0caac60267751103b591552
Arpit Singh <arpitsinghnitd@gmail.com>
2015-11-12 15:58:00 +0530

    Java

---
+++ C++_Files/big.java
@@ -0,0 +1,26 @@
+// Java program to find large factorials using BigInteger
+import java.math.BigInteger;
+import java.util.Scanner;
+ 
+public class Example
+{
+    // Returns Factorial of N
+    static BigInteger factorial(int N)
+    {
+        // Initialize result
+        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE
+ 
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= N; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+ 
+        return f;
+    }
+ 
+    // Driver method
+    public static void main(String args[]) throws Exception
+    {
+        int N = 20;
+        System.out.println(factorial(N));
+    }
+}
