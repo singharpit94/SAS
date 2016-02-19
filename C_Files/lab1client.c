6af3f7fc9ecccfe3db22004e570a40b755a313a6
Arpit Singh <arpitsinghnitd@gmail.com>
2016-02-19 15:47:07 +0530

    [Code Added] Server client normal message system

---
+++ C_Files/lab1client.c
@@ -0,0 +1,97 @@
+/*
+* Program for client 
+* Written by Arpit Kumar Singh (13/CS/31)
+            and Neetesh Kumar(13/CS/28)
+            
+            */
+
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+
+main()
+{
+	int sockfd ;
+	struct sockaddr_in serv_addr;
+	int i;
+               int temp;
+               int length;
+	char input_buffer[100];
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{
+		printf("Unable to create socket\n");
+		exit(0);
+	}
+
+	serv_addr.sin_family= AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	serv_addr.sin_port=htons(6280);
+
+	for(i=0; i < 100; i++) 
+		input_buffer[i] = '\0';
+
+
+	if ((connect(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr))) < 0) 
+	{
+		printf("Unable to connect to server\n");
+		exit(0);
+	}
+	printf("Connected with server\n");
+	printf("Server got connection from server %s\n", inet_ntoa(serv_addr.sin_addr));
+
+	while(1)
+	{
+		printf("Enter the message to send to the server");
+		scanf("%s",input_buffer);
+		//Sending the expression to server to compute the expression
+		length=0;
+		while(length<strlen(input_buffer))
+		{
+			printf("Sending to server\n***********************\n");
+			temp=send(sockfd, input_buffer, strlen(input_buffer) + 1, 0);
+			if(temp<0)
+			{
+				printf("Error in sending to server\n");
+				exit(0);
+			}
+			length = length + temp;
+				
+		}
+			
+	
+
+		//Receiving the Result from the server
+		length=0;
+		while(length<strlen(input_buffer))
+		{
+			printf("Recieving from server\n\n");
+			temp=recv(sockfd, input_buffer, 100, 0);
+			if(temp<0)
+			{
+				printf("Error in reciving from server\n");
+				exit(0);
+			}
+			length = length + temp;
+			
+				
+				
+		}
+			
+			
+	
+		printf("Value recieved from the server:");
+		printf("%s\n",input_buffer);
+		printf("\n");
+		
+	}
+	close(sockfd);
+	
+	
+}
