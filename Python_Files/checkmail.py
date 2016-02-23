e49bd49fb989af95bb9ec11210c71c00d31d0d2c
Arpit Singh <arpitsinghnitd@gmail.com>
2016-02-23 18:20:43 +0530

    Python code to check email

---
+++ Python_Files/checkmail.py
@@ -0,0 +1,15 @@
+import poplib
+from email import parser
+
+pop_conn = poplib.POP3_SSL('pop.gmail.com')
+pop_conn.user('arpitsinghnitd')
+pop_conn.pass_('')
+#Get messages from server:
+messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
+# Concat message pieces:
+messages = ["\n".join(mssg[1]) for mssg in messages]
+#Parse message intom an email object:
+messages = [parser.Parser().parsestr(mssg) for mssg in messages]
+for message in messages:
+    print message['subject']
+pop_conn.quit()
